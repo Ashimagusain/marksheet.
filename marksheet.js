c5f3532ab22ee9272c55f569e21875af6def7174
Ashimagusain <125851174+Ashimagusain@users.noreply.github.com>
2023-03-18 01:09:32 +0530

    Add files via upload

---
+++ marksheet.js
@@ -0,0 +1,17 @@
+// function calculate()
+// var a=parseInt(document.getElementById('grade1').value);
+// var a=parseInt(document.getElementById('grade2').value);
+// var a=parseInt(document.getElementById('grade3').value);
+// var a=parseInt(document.getElementById('grade4').value);
+// var a=parseInt(document.getElementById('grade5').value)
+// if (a>100 || b>100 || c>100 || d>100 || e>100 || )
+// {
+//     alert("please enter correct value");
+// }
+// else
+// {
+//     var t =a+b+c+d+e;
+//     document.getElementById
+// }
+
+
